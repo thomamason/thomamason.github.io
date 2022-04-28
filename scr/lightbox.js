5ceb4f558bedab0c1b1dfa7eab7bc37de27f12aa
Thomas Featherstone <51062538+thomamason@users.noreply.github.com>
2022-04-28 20:42:39 +0100

    final commit for the day

---
+++ scr/lightbox.js
@@ -0,0 +1,8 @@
+function openModal() {
+    document.getElementById("modal").setAttribute('style', 'display:block;');
+  }
+  
+  // Close the Modal
+  function closeModal() {
+    document.getElementsByClassName("modal").style.display = "none";
+  }
