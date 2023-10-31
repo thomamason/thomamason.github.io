0b62fab40ce0bdf1028f96203f97ce208028dd05
Thomas Featherstone <51062538+thomamason@users.noreply.github.com>
2023-10-31 10:56:44 +0000

    Adds dropdown element for BC banner
    includes styling and scripting

---
+++ scr/scripts.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const dropdownIcon = document.getElementById("dropdown-icon");
+    const mediaDropdown = document.getElementById("media-dropdown");
+
+    dropdownIcon.addEventListener("click", function () {
+        if (mediaDropdown.style.display === "block") {
+            mediaDropdown.style.display = "none";
+        } else {
+            mediaDropdown.style.display = "block";
+        }
+    });
+
+    // Close the dropdown when clicking outside of it
+    window.addEventListener("click", function (event) {
+        if (event.target != dropdownIcon) {
+            mediaDropdown.style.display = "none";
+        }
+    });
+});
