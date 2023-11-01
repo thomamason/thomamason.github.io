71aeef39d6aba99604d4569efed1a932e08ab2c9
Thomas Featherstone <51062538+thomamason@users.noreply.github.com>
2023-11-01 13:19:41 +0000

    clears up old code

--- scr/dist/scripts.dev.js
+++ scr/dist/scripts.dev.js
@@ -1,8 +1,12 @@
-"use strict";
+'use strict';
 
 document.addEventListener("DOMContentLoaded", function () {
+  // Initialize your variables
   var dropdownIcon = document.getElementById("bc-banner");
   var mediaDropdown = document.getElementById("media-dropdown");
+  var video = document.getElementById('jaiVideo');
+  var loadingScreen = document.getElementById('loadingScreen');
+  var lazyloadThrottleTimeout;
   dropdownIcon.addEventListener("click", function () {
     if (mediaDropdown.style.display === "flex") {
       mediaDropdown.style.display = "none";
@@ -16,4 +20,35 @@ document.addEventListener("DOMContentLoaded", function () {
       mediaDropdown.style.display = "none";
     }
   });
+
+  function playVideo() {
+    if (!video.paused) {
+      video.play();
+    } else {
+      video.style.display = 'block';
+      loadingScreen.style.display = 'none'; // Set opacity to 1 to trigger the fade-in effect
+
+      video.style.opacity = 1;
+      video.addEventListener('canplaythrough', function () {
+        // Video has loaded, hide the loading screen and show the video
+        loadingScreen.style.display = 'none';
+        video.play();
+      });
+    }
+  }
+
+  function initCloudinary() {
+    var cl = cloudinary.Cloudinary["new"]({
+      cloud_name: '<Cloud Name>'
+    });
+    cl.responsive();
+  }
+
+  function lazyLoadImages() {} // Your lazy load image code here
+  // Attach event listeners
+
+
+  document.body.addEventListener("touchstart", playVideo);
+  initCloudinary();
+  lazyLoadImages();
 });
